bafd3a19dec8c04b14b6b086faa8f63db7b55822
Megan Comeau <mlcomeau99@gmail.com>
2020-05-19 02:43:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,11 @@ class Show < ActiveRecord::Base
   def Show::highest_rating
     Show.maximum("rating")
   end
+
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+  end
+
+  def Show::lowest_rating
+    Show.minimum("rating")
 end
